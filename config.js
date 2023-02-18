b1444fcf263b6ce73a61109b38cf13b66b5673c3
replit100 <125527262+replit100@users.noreply.github.com>
2023-02-18 13:29:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
-LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
+LANG: process.env.LANG === undefined ? 'SI' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
 FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? 'REPLIT' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=J7BxHb5Z#DKUGA0eKB9naFowx4T0HYwWHpHRBqRRgEtVh8q3KQlo' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=ojQVmawS#aK-qAxCxpDCqjEpbtDgEOTjfBI-BBo1XqdblnjH1kNI' : process.env.SESSION_ID
 };
